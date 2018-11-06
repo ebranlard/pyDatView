8703baa2400f19f0a6f67f7f395f473c5d154d75
Emmanuel Branlard <elmanuelito.github@gmail.com>
2018-11-05 21:12:36 -0700

    Adding performance test with 10x10M data points (Closes ./.travis.yml)

--- pyDatView.py
+++ pyDatView.py
@@ -12,25 +12,31 @@ def main(inputfiles=[]):
 
 
 def tests():
-    # for now only weio tests
-    #import pydatview
-    #pydatview.test()
+    import platform
+    if platform.system()=='Windows':
+        from pydatview.perfmon import Timer
+        with Timer('Main import'):
+            import pydatview
+        with Timer('Test'):
+            pydatview.test()
 
-    import weio 
-    import glob
-    Files = glob.glob('_tests/*')+ glob.glob('weio/_tests/*')
-    for f in Files:
-        try:
-            F = weio.read(f)
-            s=F.formatName()
-            s=s[:s.find('(')].replace('file','')[:20]
-            print('[ OK ] {:30s}\t{:20s}'.format(f[:30],s))
-        except weio.FormatNotDetectedError:
-            print('[FAIL] {:30s}\tFormat not detected'.format(f[:30]))
-            raise
-        except:
-            print('[FAIL] {:30s}\tException occured'.format(f[:30]))
-            raise
+    else:
+        # for now only weio tests
+        import weio 
+        import glob
+        Files = glob.glob('_tests/*')+ glob.glob('weio/_tests/*')
+        for f in Files:
+            try:
+                F = weio.read(f)
+                s=F.formatName()
+                s=s[:s.find('(')].replace('file','')[:20]
+                print('[ OK ] {:30s}\t{:20s}'.format(f[:30],s))
+            except weio.FormatNotDetectedError:
+                print('[FAIL] {:30s}\tFormat not detected'.format(f[:30]))
+                raise
+            except:
+                print('[FAIL] {:30s}\tException occured'.format(f[:30]))
+                raise
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
