a2ea215ded2c73ed7178b50d79b9a49678977a1d
Emmanuel Branlard <elmanuelito.github@gmail.com>
2019-06-23 20:23:04 -0600

    _removed unnecessary code

--- pydatview/GUISelectionPanel.py
+++ pydatview/GUISelectionPanel.py
@@ -821,38 +821,6 @@ class SelectionPanel(wx.Panel):
             else:
                 print('Tab',i,'xSel:',TS[tn]['xSel'],'ySel:',TS[tn]['ySel'],'Name:',tn)
 
-    def getFullSelection(self):
-        ID = []
-        iX1=None; IY1=None; sX1=None; SY1=None;
-        iX2=None; IY2=None; sX2=None; SY2=None;
-        ITab=None;
-        STab=None;
-        SameCol=False
-        if hasattr(self,'tabs') and len(self.tabs)>0:
-            ITab,STab = self.getSelectedTables()
-            iX1,IY1,sX1,SY1 = self.colPanel1.getColumnSelection()
-            if self._mode =='sameColumnsMode':
-                SameCol=True
-                for i,(itab,stab) in enumerate(zip(ITab,STab)):
-                    for j,(iy,sy) in enumerate(zip(IY1,SY1)):
-                        ID.append([itab,iX1,iy,sX1,sy,stab])
-            elif self._mode =='twoColumnsMode':
-                SameCol=haveSameColumns(self.tabs,ITab)
-                if len(ITab)>=1:
-                    for j,(iy,sy) in enumerate(zip(IY1,SY1)):
-                        ID.append([ITab[0],iX1,iy,sX1,sy,STab[0]])
-                if len(ITab)>=2:
-                    if SameCol:
-                        iX2=iX1;IY2=IY1;sX2=sX1;SY2=SY1;
-                    else:
-                        iX2,IY2,sX2,SY2 = self.colPanel2.getColumnSelection()
-                    for j,(iy,sy) in enumerate(zip(IY2,SY2)):
-                        ID.append([ITab[1],iX2,iy,sX2,sy,STab[1]])
-            else:
-                raise Exception('Unknown mode {}'.format(self._mode))
-        return ID,ITab,iX1,IY1,iX2,IY2,STab,sX1,SY1,sX2,SY2,SameCol
-
-
     def getPlotDataSelection(self):
         ID = []
         SameCol=False
