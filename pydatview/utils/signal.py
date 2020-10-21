7076b5405643bfd16faf1c2351c63f1b7ef65d6a
Emmanuel Branlard <elmanuelito.github@gmail.com>
2020-10-21 16:01:53 -0600

    Update of weio and using NaN for removed outliers

--- pydatview/utils/signal.py
+++ pydatview/utils/signal.py
@@ -1,25 +1,27 @@
 import numpy as np
 
-def reject_outliers(y, x=None, m = 2.):
-    """ Reject outliers """
+def reject_outliers(y, x=None, m = 2., replaceNaN=True):
+    """ Reject outliers:
+        If replaceNaN is true: they are replaced by NaN 
+        Otherwise they are removed
+    """
     if m==0: 
         # No rejection...
-        if x is None:
-            return y
-        else:
-            return x, y
-
-    dd = np.abs(y - np.nanmedian(y))
-    mdev = np.nanmedian(dd)
-    if mdev:
-        ss = dd/mdev 
-        b=ss<m
-        if x is None:
-            return y[b]
-        else:
-            return x[b], y[b]
+        pass
     else:
-        if x is None:
-            return y
-        else:
-            return x, y
+        dd = np.abs(y - np.nanmedian(y))
+        mdev = np.nanmedian(dd)
+        if mdev:
+            ss = dd/mdev 
+            b=ss<m
+            if replaceNaN:
+                y=y.copy()
+                y[~b]=np.nan
+            else:
+                y=y[b]
+                if x is not None:
+                    x= x[b]
+    if x is None:
+        return y
+    else:
+        return x, y
