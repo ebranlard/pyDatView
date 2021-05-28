b01ff671089f8102d9d5d5fb3c4c532b0c1bf1b2
Emmanuel Branlard <elmanuelito.github@gmail.com>
2021-05-28 14:14:53 -0600

    Adding tools-tests

---
+++ tests/test_damping.py
@@ -0,0 +1,6 @@
+import unittest
+import numpy as np
+from pydatview.tools.damping import *
+ 
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_spectral.py
@@ -0,0 +1,6 @@
+import unittest
+import numpy as np
+from pydatview.tools.spectral import TestSpectral
+
+if __name__ == '__main__':
+    unittest.main()
