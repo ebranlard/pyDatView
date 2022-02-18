FFT: option to average using n points per decade (Fixed #75)

pydatview/GUIPlotPanel.py
@@ -113,7 +113,7 @@ class SpectralCtrlPanel(wx.Panel):
         self.cbType            = wx.ComboBox(self, choices=['PSD','f x PSD','Amplitude'] , style=wx.CB_READONLY)
         self.cbType.SetSelection(0)
         lbAveraging            = wx.StaticText( self, -1, 'Avg.:')
-        self.cbAveraging       = wx.ComboBox(self, choices=['None','Welch'] , style=wx.CB_READONLY)
+        self.cbAveraging       = wx.ComboBox(self, choices=['None','Welch','Binning'] , style=wx.CB_READONLY)
         self.cbAveraging.SetSelection(1)
         self.lbAveragingMethod = wx.StaticText( self, -1, 'Window:')
         self.cbAveragingMethod = wx.ComboBox(self, choices=['Hamming','Hann','Rectangular'] , style=wx.CB_READONLY)
@@ -121,7 +121,9 @@ class SpectralCtrlPanel(wx.Panel):
         self.lbP2 = wx.StaticText( self, -1, '2^n:')
         self.scP2 = wx.SpinCtrl(self, value='11',size=wx.Size(40,-1))
         self.lbWinLength = wx.StaticText( self, -1, '(2048) ')
-        self.scP2.SetRange(3, 19)
+        self.scP2.SetRange(3, 50)
+        self.previousNExp = 8
+        self.previousNDec = 20
         lbMaxFreq     = wx.StaticText( self, -1, 'Xlim:')
         self.tMaxFreq = wx.TextCtrl(self,size = (30,-1),style=wx.TE_PROCESS_ENTER)
         self.tMaxFreq.SetValue("-1")
@@ -155,13 +157,36 @@ class SpectralCtrlPanel(wx.Panel):
     def onXlimChange(self,event=None):
         self.parent.redraw_same_data();
     def onSpecCtrlChange(self,event=None):
+        if self.cbAveraging.GetStringSelection()=='None':
+            self.scP2.Enable(False)
+            self.cbAveragingMethod.Enable(False)
+            self.lbP2.SetLabel('')
+            self.lbWinLength.SetLabel('')
+        elif self.cbAveraging.GetStringSelection()=='Binning':
+            self.previousNExp= self.scP2.GetValue()
+            self.scP2.SetValue(self.previousNDec)
+            self.scP2.Enable(True)
+            self.cbAveragingMethod.Enable(False)
+            self.lbP2.SetLabel('n:')
+            self.lbWinLength.SetLabel('')
+        else:
+            self.previousDec= self.scP2.GetValue()
+            self.scP2.SetValue(self.previousNExp)
+            self.lbP2.SetLabel('2^n:')
+            self.scP2.Enable(True)
+            self.cbAveragingMethod.Enable(True)
+            self.onP2ChangeText(event=None)
         self.parent.load_and_draw() # Data changes
+
     def onDetrendChange(self,event=None):
         self.parent.load_and_draw() # Data changes
 
     def onP2ChangeText(self,event=None):
-        nExp=self.scP2.GetValue()
-        self.updateP2(nExp)
+        if self.cbAveraging.GetStringSelection()=='Binning':
+            pass
+        else:
+            nExp=self.scP2.GetValue()
+            self.updateP2(nExp)
         self.parent.load_and_draw() # Data changes
 
     def updateP2(self,P2):
@@ -683,7 +708,7 @@ class PlotPanel(wx.Panel):
                         self.rightMeasure.plot(ax, ax_idx)
                     else:
                         return
-                    if self.cbAutoScale.IsChecked() is False:
+                    if not self.cbAutoScale.IsChecked():
                         self._restore_limits()
                         
                     if self.leftMeasure.axis_idx == self.rightMeasure.axis_idx and self.leftMeasure.axis_idx != -1:
@@ -770,9 +795,10 @@ class PlotPanel(wx.Panel):
         avgWindow  = self.spcPanel.cbAveragingMethod.GetStringSelection()
         bDetrend   = self.spcPanel.cbDetrend.IsChecked()
         nExp       = self.spcPanel.scP2.GetValue()
+        nPerDecade = self.spcPanel.scP2.GetValue()
         # Convert plotdata to FFT data
         try:
-            Info = pd.toFFT(yType=yType, xType=xType, avgMethod=avgMethod, avgWindow=avgWindow, bDetrend=bDetrend, nExp=nExp) 
+            Info = pd.toFFT(yType=yType, xType=xType, avgMethod=avgMethod, avgWindow=avgWindow, bDetrend=bDetrend, nExp=nExp, nPerDecade=nPerDecade) 
             # Trigger
             if hasattr(Info,'nExp') and Info.nExp!=nExp:
                 self.spcPanel.scP2.SetValue(Info.nExp)
@@ -1013,18 +1039,21 @@ class PlotPanel(wx.Panel):
             # XLIM - TODO FFT ONLY NASTY
             if self.pltTypePanel.cbFFT.GetValue():
                 try:
-                    xlim=float(self.spcPanel.tMaxFreq.GetLineText(0))
-                    if xlim>0:
-                        ax_left.set_xlim([0,xlim])
-                        pd=PD[ax_left.iPD[0]]
-                        I=pd.x<xlim
-                        ymin = np.min([np.min(PD[ipd].y[I]) for ipd in ax_left.iPD])
-                        ax_left.set_ylim(bottom=ymin/2)
-                    if self.spcPanel.cbTypeX.GetStringSelection()=='x':
-                        ax_left.invert_xaxis()
+                    if self.cbAutoScale.IsChecked():
+                        xlim=float(self.spcPanel.tMaxFreq.GetLineText(0))
+                        if xlim>0:
+                                ax_left.set_xlim([0,xlim])
+                                pd=PD[ax_left.iPD[0]]
+                                I=pd.x<xlim
+                                ymin = np.min([np.min(PD[ipd].y[I]) for ipd in ax_left.iPD])
+                                ax_left.set_ylim(bottom=ymin/2)
+                        if self.spcPanel.cbTypeX.GetStringSelection()=='x':
+                            ax_left.invert_xaxis()
+                    else:
+                        self._restore_limits()
                 except:
                     pass
-            elif self.cbAutoScale.IsChecked() is False and keep_limits:
+            elif not self.cbAutoScale.IsChecked() and keep_limits:
                 self._restore_limits()
 
             ax_left.grid(self.cbGrid.IsChecked())

pydatview/tools/spectral.py
@@ -20,6 +20,7 @@
 from __future__ import division, print_function, absolute_import
 
 import numpy as np
+import pandas as pd
 from six import string_types
 
 __all__  = ['fft_wrap','welch', 'psd', 'fft_amplitude']
@@ -32,12 +33,12 @@ __all__ += ['TestSpectral']
 # --------------------------------------------------------------------------------}
 # --- FFT wrap
 # --------------------------------------------------------------------------------{
-def fft_wrap(t,y,dt=None, output_type='amplitude',averaging='None',averaging_window='hamming',detrend=False,nExp=None):
+def fft_wrap(t,y,dt=None, output_type='amplitude',averaging='None',averaging_window='hamming',detrend=False,nExp=None, nPerDecade=None):
     """ 
     Wrapper to compute FFT amplitude or power spectra, with averaging.
     INPUTS:
        output_type      : amplitude, PSD, f x PSD
-       averaging_method : None, Welch
+       averaging : None, Welch, Binning
        averaging_window : Hamming, Hann, Rectangular
     OUTPUTS:
        frq: vector of frequencies
@@ -60,8 +61,10 @@ def fft_wrap(t,y,dt=None, output_type='amplitude',averaging='None',averaging_win
         if dtDelta0 !=dt:
             print('[WARN] dt from tmax-tmin different from dt from t2-t1' )
     Fs = 1/dt
-    if averaging=='none':
+    if averaging =='none':
         frq, PSD, Info = psd(y, fs=Fs, detrend=detrend, return_onesided=True)
+    elif averaging =='binning':
+        frq, PSD, Info = psd_binned(y, fs=Fs, detrend=detrend, return_onesided=True, nPerDecade=nPerDecade)
     elif averaging=='welch':
         # --- Welch - PSD
         #overlap_frac=0.5
@@ -114,6 +117,70 @@ def fft_amplitude(y, fs=1.0, detrend ='constant', return_onesided=True):
     deltaf = frq[1]-frq[0]
     Y = np.sqrt(PSD*2*deltaf)
     return frq, Y, Info
+
+
+def psd_binned(y, fs=1.0, nPerDecade=10, detrend ='constant', return_onesided=True):
+    """ 
+    Return PSD binned with nPoints per decade
+    """
+    # --- First  return regular PSD
+    frq, PSD, Info = psd(y, fs=fs, detrend=detrend, return_onesided=return_onesided)
+
+    add0=False
+    if frq[0]==0:
+        add0=True
+        f0   = 0
+        PSD0 = PSD[0]
+        frq=frq[1:]
+        PSD=PSD[1:]
+
+    # -- Then bin per decase
+    log_f = np.log10(frq)
+    ndecades = np.ceil(log_f[-1] -log_f[0])
+    xbins = np.linspace(log_f[0], log_f[-1], int(ndecades*nPerDecade))
+
+    # Using Pandas to bin..
+    df = pd.DataFrame(data=np.column_stack((log_f,PSD)), columns=['x','y'])
+    xmid  = (xbins[:-1]+xbins[1:])/2
+    df['Bin'] = pd.cut(df['x'], bins=xbins, labels=xmid ) # Adding a column that has bin attribute
+    df2  = df.groupby('Bin').mean()                     # Average by bin
+    df2  = df2.reindex(xmid)
+    log_f_bin = df2['x'].values
+    PSD_bin = df2['y'].values
+    frq2= 10**log_f_bin
+    PSD2= PSD_bin
+    if add0:
+        frq2=np.concatenate(  ([f0  ], frq2)  )
+        PSD2=np.concatenate(  ([PSD0], PSD2)  )
+    b = ~np.isnan(frq2)
+    frq2 = frq2[b]
+    PSD2 = PSD2[b]
+
+    #import matplotlib.pyplot as plt
+    #fig,ax = plt.subplots(1, 1, sharey=False, figsize=(6.4,4.8)) # (6.4,4.8)
+    #fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
+    #ax.plot(log_f, PSD, label='')
+    #ax.plot(log_f_bin, PSD_bin, 'o', label='')
+    #for x in xbins:
+    #    ax.axvline(x, ls=':', c=(0.5,0.5,0.5))
+    #ax.set_xlabel('')
+    #ax.set_ylabel('')
+    #ax.legend()
+    #plt.show()
+
+    #Info.df    = frq[1]-frq[0]
+    #Info.fMax  = frq[-1]
+    #Info.LFreq = len(frq)
+    #Info.LSeg  = len(Y)
+    #Info.LWin  = len(Y)
+    #Info.LOvlp = 0
+    #Info.nFFT  = len(Y)
+    #Info.nseg  = 1
+    Info.nPerDecade  = nPerDecade
+    Info.xbins  = xbins
+
+    return frq2, PSD2, Info
+
 
 def psd(y, fs=1.0, detrend ='constant', return_onesided=True):
     """ Perform PSD without averaging """
@@ -955,7 +1022,30 @@ class TestSpectral(unittest.TestCase):
         i=np.argmax(Y)
         self.assertAlmostEqual(Y[i],A)
         self.assertAlmostEqual(f[i],f0)
+
+    def test_fft_binning(self):
+        dt=0.1
+        t=np.arange(0,10,dt);
+        f0=1;
+        A=5;
+        y=A*np.sin(2*np.pi*f0*t)
+
+        f,   Y, Info  = psd_binned(y, fs=1/dt, nPerDecade=10, detrend ='constant')
+        f2, Y2, Info2 = psd       (y, fs=1/dt,           detrend ='constant')
+        #print(f)
+        #print(Y)
+
+        #import matplotlib.pyplot as plt
+        #fig,ax = plt.subplots(1, 1, sharey=False, figsize=(6.4,4.8)) # (6.4,4.8)
+        #fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
+        #ax.plot( f2, Y2   , label='Full')
+        #ax.plot( f,  Y    , label='Binned')
+        #ax.set_xlabel('')
+        #ax.set_ylabel('')
+        #ax.legend()
+        #plt.show()
     
 if __name__ == '__main__':
+    #TestSpectral().test_fft_binning()
     unittest.main()
 