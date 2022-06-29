5ca1b30b03fd9664eee74f7776876ef9e263ef55
Emmanuel Branlard <elmanuelito.github@gmail.com>
2022-06-29 14:23:03 -0600

    --amend

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import pandas as pd
-from pydatview.tools.signal import *
+from pydatview.tools.signal_analysis import *
 
 # --------------------------------------------------------------------------------}
 # ---  
