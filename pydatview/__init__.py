e7d485805f98bf90fcb67d046940c1e1b794cb97
Emmanuel Branlard <elmanuelito.github@gmail.com>
2022-09-15 15:37:21 -0600

    Changing exe entry point to include arguments

--- pydatview/__init__.py
+++ pydatview/__init__.py
@@ -1,8 +1,14 @@
-__all__ = ['show']
+__all__ = ['show', 'show_sys_args']
 
 # defining main function here, to avoid import of pydatview and wx of some unittests
 def show(*args,**kwargs):
     from pydatview.main import showApp
     showApp(*args,**kwargs)
 
+def show_sys_args():
+    import sys
+    if len(sys.argv)>1:
+        show(filenames=sys.argv[1:])
+    else:
+        show()
 
