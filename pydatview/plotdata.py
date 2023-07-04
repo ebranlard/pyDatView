32e97332bd1a8b06b9cc19c9f8a649978f3572e0
SimonHH <38310787+SimonHH@users.noreply.github.com>
2023-07-04 12:04:03 +0200

    fix plotdata.py for updates in fatigue.py

--- pydatview/plotdata.py
+++ pydatview/plotdata.py
@@ -630,7 +630,7 @@ class PlotData():
             except ModuleNotFoundError:
                 print('[INFO] module fatpack not installed, default to windap method for equivalent load')
                 method='rainflow_windap'
-            v=equivalent_load(PD.y, m=m, Teq=T, nBins=100, method=method)
+            v=equivalent_load(PD.x, PD.y, m=m, Teq=T, nBins=100, method=method)
             return v,pretty_num(v)
 
     def Info(PD,var):
